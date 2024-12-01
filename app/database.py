101ec70eab29b0fbc92e67a3dbe7ded1196cbf0b
Augusto <augusto@cedeno.com>
2024-12-01 10:33:24 -0300

    ajustes

--- app/database.py
+++ app/database.py
@@ -5,24 +5,3 @@ def get_connection():
     engine = create_engine('sqlite:///my_database.db', echo=True)
     session = sessionmaker(bind=engine)
     return session()
-
-# def pydantic_to_sqlalchemy(pydantic_obj: TriviaUserPydantic) -> TriviaUserAlchemy:
-#     # Convierte un objeto Pydantic a un dictionary 
-#     return TriviaUserAlchemy(**pydantic_obj.dict())
-
-# def add_item(trivia_user: TriviaUserPydantic):
-#     conexion = None
-#     try:
-#         item = pydantic_to_sqlalchemy(trivia_user)
-#         conexion = get_connection()
-#         conexion.add(item)
-#         conexion.commit()
-#         conexion.commit()
-#         return item.id
-#     except Exception as ex:
-#         if conexion and hasattr(conexion, 'rollback'):
-#             conexion.rollback()
-#         print(ex)
-#     finally:
-#         if conexion:
-#             conexion.close()
